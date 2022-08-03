7b6d5ef517a29f73014c22e515d1e32a6b07216a
seo-yeongjun <zanygeek8371@gmail.com>
2022-08-03 20:24:14 +0900

    Initial commit

---
+++ .idea/메모.py
@@ -0,0 +1,10 @@
+# from itertools import product        중복 순열
+# from itertools import permutations     순열
+# from itertools import combinations     조합
+# from itertools import combinations_with_replacement    중복 조합
+# from collections import Counter 리스트 내 원소 수 세기
+# import math     // gcd 최대공약수    ,   lcm 최소 공배수
+#  a = (1,)  튜플
+#  a = [] 리스트
+#  a =  { 1:2,} 딕셔너리
+#  a = set() 셋
