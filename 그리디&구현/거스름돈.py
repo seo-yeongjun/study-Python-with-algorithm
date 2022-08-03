c65d631ae171eb92d51324b84c287d4b18cf1ce4
seo-yeongjun <zanygeek8371@gmail.com>
2022-08-03 23:48:48 +0900

    그리디

--- 그리디&구현/거스름돈.py
+++ 그리디&구현/거스름돈.py
@@ -1,6 +1,8 @@
-잔돈 = int(input())
+# https://www.acmicpc.net/problem/5585
+
+잔돈 = 1000 - int(input())
 답 = 0
-arr = [500, 100, 50, 10]
+arr = [500, 100, 50, 10, 5, 1]
 
 for a in arr:
     답 += 잔돈 // a
